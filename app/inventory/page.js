bee4c78da9c70216c134b2236db54b1480d9468d
Raj Patel <raj84908@gmail.com>
2025-08-07 20:23:03 -0400

    testing for bitbucket

--- app/inventory/page.js
+++ app/inventory/page.js
@@ -35,6 +35,10 @@ export default function InventoryPage() {
 
         return () => clearTimeout(timer);
     }, [searchTerm]);
+    
+    
+    
+    
 
     // Fetch inventory data
     const fetchInventory = useCallback(async (search = "", category = "all") => {
